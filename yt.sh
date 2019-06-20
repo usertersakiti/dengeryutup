558f4a324f4d386742d7f2cd7db062250f8c9a49
usertersakiti <51637256+usertersakiti@users.noreply.github.com>
2019-06-20 16:36:26 +0700

    Add files via upload

---
+++ yt.sh
@@ -0,0 +1,37 @@
+#User_Tersakiti
+
+
+
+
+#Gitu aja rikode
+
+
+
+
+#
+
+
+
+
+
+echo "--------------------"
+echo "User Tersakiti"
+echo "Indonesia Coder"
+echo "---------------------"
+
+sleep 2
+
+echo "Installing...."
+
+pkg install python
+pip install mps_youtube
+pip install youtube_dl
+pkg install mpv
+
+clear
+
+sleep 1
+
+echo "Installing Selesai"
+
+mpsyt
